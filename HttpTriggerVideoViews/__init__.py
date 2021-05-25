3ea4e11259de6bfee314b9c2d5155aa92ce7d64b
OD1995 <oliverdernie1@googlemail.com>
2021-05-25 14:16:31 +0100

    both fb and insta working

--- HttpTriggerVideoViews/__init__.py
+++ HttpTriggerVideoViews/__init__.py
@@ -34,7 +34,18 @@ def save_text(
             container_name="test",
             blob_name=f"{rowID} JS.json",
             text=str(html)
-        )       
+        )
+
+def get_script_from_soup(soup):
+    scripts = [x
+              for x in soup.find_all('script',{'type':'text/javascript'})
+              if "video_view_count" in str(x)]
+    if len(scripts) == 0:
+        return False,""
+    elif len(scripts) == 1:
+        return True,scripts[0]
+    else:
+        raise ValueError("`video_view_count` found in more than one script")
 
 def insta_get_video_views(session,URL,mediaType,rowID,UA):
     H = {"user-agent" : UA}
@@ -46,49 +57,52 @@ def insta_get_video_views(session,URL,mediaType,rowID,UA):
         rowID=rowID
     )
     soup = BS(html,'html.parser')
-    script = [x
-              for x in soup.find_all('script',{'type':'text/javascript'})
-              if "video_view_count" in str(x)][0]
-    if "window._sharedData" in str(script):
-        logging.info("window._sharedData")
-        dataStr = str(script).replace(
-            '<script type="text/javascript">window._sharedData = ',
-            ''
-        ).replace(
-            ';</script>',
-            ''
+    scriptFound,script = get_script_from_soup(soup)
+    if scriptFound:
+        if "window._sharedData" in str(script):
+            logging.info("window._sharedData")
+            dataStr = str(script).replace(
+                '<script type="text/javascript">window._sharedData = ',
+                ''
+            ).replace(
+                ';</script>',
+                ''
+            )
+            data = json.loads(dataStr)
+            shortcodeMedia = data['entry_data']['PostPage'][0]['graphql']['shortcode_media']
+        elif "window.__additionalDataLoaded" in str(script):
+            logging.info("window.__additionalDataLoaded")
+            postBase = URL.replace(
+                "https://www.instagram.com",
+                ""
+            )
+            dataStr = str(script).replace(
+                f"""<script type="text/javascript">window.__additionalDataLoaded('{postBase}',""",
+                ""
+            ).replace(
+                ');</script>',
+                ''
+            )
+            data = json.loads(dataStr)
+            shortcodeMedia = data['graphql']['shortcode_media']
+        save_text(
+            html=data,
+            rowID=rowID,
+            _format_="js"
         )
-        data = json.loads(dataStr)
-        shortcodeMedia = data['entry_data']['PostPage'][0]['graphql']['shortcode_media']
-    elif "window.__additionalDataLoaded" in str(script):
-        logging.info("window.__additionalDataLoaded")
-        postBase = URL.replace(
-            "https://www.instagram.com",
-            ""
-        )
-        dataStr = str(script).replace(
-            f"""<script type="text/javascript">window.__additionalDataLoaded('{postBase}',""",
-            ""
-        ).replace(
-            ');</script>',
-            ''
-        )
-        data = json.loads(dataStr)
-        shortcodeMedia = data['graphql']['shortcode_media']
-    save_text(
-        html=data,
-        rowID=rowID,
-        _format_="js"
-    )
-    if mediaType != 'album':
-        num = shortcodeMedia['video_view_count']
+        if mediaType != 'album':
+            num = shortcodeMedia['video_view_count']
+        else:
+            num = 0
+            for edge in shortcodeMedia['edge_sidecar_to_children']['edges']:
+                if "video_view_count" in edge['node']:
+                    num += edge['node']["video_view_count"]
+        
+        return int(num)
     else:
-        num = 0
-        for edge in shortcodeMedia['edge_sidecar_to_children']['edges']:
-            if "video_view_count" in edge['node']:
-                num += edge['node']["video_view_count"]
-    
-    return int(num)
+        ## Add RowID to VideoViewsFailures
+        add_rowID_to_vvf(rowID)
+        return "no"
 
 def fb_get_video_views(session,URL,rowID,userAgent):
     H = {"user-agent" : userAgent}
@@ -157,6 +171,22 @@ def insta_login_inner(session,userAgent):
     else:
         return False
 
+def get_fb_vID(url):
+    if url[-1] == "/":
+        vID = url.split("/")[-2]
+    else:
+        vID = url.split("/")[-1]
+    return vID
+
+def add_rowID_to_vvf(rowID):
+    Q = f"INSERT INTO VideoViewsFailures ([RowID]) VALUES ({rowID})"
+    run_sql_commmand(
+        query=Q,
+        server="nonDashboard",
+        database="GlobalMultimedia"
+    )
+    logging.info(f"{rowID} added to VideoViewsFailures")
+
 def scrape_topX(X):
 
     if X is None:
@@ -181,7 +211,8 @@ LEFT JOIN			MASTER_ClientArticles M
 	ON				M.Master_PostID=O.Master_PostID
 LEFT JOIN			[Toolkit_SourceTable_CrowdTangle_Octagon] T
 	ON				M.SourceTable_LocalID = T.ImportId
-WHERE				O.VideoViews IS NULL
+WHERE				O.RowID NOT IN (SELECT RowID FROM VideoViewsFailures)
+    AND             O.VideoViews IS NULL
 ORDER BY			M.Article_Timestamp ASC
     """
     initialTopXdf = get_df_from_sqlQuery(
@@ -189,7 +220,7 @@ ORDER BY			M.Article_Timestamp ASC
         database="GlobalMultimedia"
     )
     ## Start session
-    userAgent= "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36"
+    userAgent = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36"
     
     with requests.Session() as s:
         ## Login to Instagram
@@ -209,6 +240,21 @@ ORDER BY			M.Article_Timestamp ASC
                     userAgent=userAgent
                 )
                 scrapeSuccess = isinstance(videoViews,int)
+                ## If first attempt didn't work, try again with a different URL
+                if not scrapeSuccess:
+                    vID = get_fb_vID(initialTopXdf.loc[i,"Link"])
+                    urlAttempt2 = f"https://www.facebook.com/watch/?v={vID}"
+                    videoViews = fb_get_video_views(
+                        session=s,
+                        URL=urlAttempt2,
+                        rowID=initialTopXdf.loc[i,'RowID'],
+                        userAgent=userAgent
+                    )
+                    scrapeSuccess = isinstance(videoViews,int)
+
+                    if not scrapeSuccess:
+                        add_rowID_to_vvf(initialTopXdf.loc[i,'RowID'])
+
             elif "www.instagram.com" in initialTopXdf.loc[i,"Link"]:
                 videoViews = insta_get_video_views(
                     session=s,
@@ -227,7 +273,7 @@ ORDER BY			M.Article_Timestamp ASC
                 uC = f"""
     UPDATE ObservedSocialVideoViews
     SET VideoViews = {videoViews}, DateScraped = '{dateScraped}', ScrapeCount = 1
-    WHERE Master_PostID = '{initialTopXdf.loc[i,"Master_PostID"]}'            
+    WHERE Master_PostID = '{initialTopXdf.loc[i,"Master_PostID"]}'        
                 """
                 run_sql_commmand(
                     query=uC,
